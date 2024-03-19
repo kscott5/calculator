e35b651ea845ad6d9f1a4ce60de3bacd85505634
Karega K. Scott <kkscott@outlook.com>
2024-03-19 11:27:31 -0500

    Learning Go Inteferace with Microsoft

--- geometry.go
+++ geometry.go
@@ -1,5 +1,10 @@
 package calculator
 
+type Shape interface {
+	Perimeter() float64
+	Area() float64
+}
+
 type Triangle struct {
 	size int
 }
@@ -19,4 +24,16 @@ func (t Triangle) Size() int {
 func (t *Triangle) Perimeter() int {
 	t.doubleSize()
 	return t.size * 3
+}
+
+type Square struct {
+	size float64
+}
+
+func (s Square) Area() float64 {
+	return s.size * s.size
+}
+
+func (s Square) Perimeter() float64 {
+	return s.size * 4
 }
