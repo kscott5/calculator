50b95a4224f5190081ed1bd96897b09bf779f7bc
Karega K. Scott <kkscott@outlook.com>
2024-03-19 11:56:01 -0500

    Cirle type

--- geometry.go
+++ geometry.go
@@ -1,5 +1,7 @@
 package calculator
 
+import "math"
+
 type Shape interface {
 	Perimeter() float64
 	Area() float64
@@ -27,13 +29,25 @@ func (t *Triangle) Perimeter() int {
 }
 
 type Square struct {
-	size float64
+	Size float64
 }
 
 func (s Square) Area() float64 {
-	return s.size * s.size
+	return s.Size * s.Size
 }
 
 func (s Square) Perimeter() float64 {
-	return s.size * 4
+	return s.Size * 4
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
 }
